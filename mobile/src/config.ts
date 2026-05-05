e5b1cabefd003af909d1537c701be7bd51fbf8e9
DrianSAN0 <adrian.sanchez.nina@gmail.com>
2026-05-04 21:30:59 -0400

    feat: API Gateway URL en config.ts

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -6,6 +6,6 @@
 const IS_LOCAL = false;
 
 const LOCAL_URL = 'http://127.0.0.1:8000';
-const AWS_URL   = 'http://3.144.245.237:8000';
+const AWS_URL   = 'https://qeznnpvn51.execute-api.us-east-2.amazonaws.com';
 
 export const BACKEND_URL = IS_LOCAL ? LOCAL_URL : AWS_URL;
